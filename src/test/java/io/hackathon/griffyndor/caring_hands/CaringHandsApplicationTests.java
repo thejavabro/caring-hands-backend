4d069eeba3f92d16d06190570cf85ac9d0ec590e
thejavabro <akashthecoder@gmail.com>
2024-07-19 03:26:17 +0530

    integration with cloudSQL

---
+++ src/test/java/io/hackathon/griffyndor/caring_hands/CaringHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hackathon.griffyndor.caring_hands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringHandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
